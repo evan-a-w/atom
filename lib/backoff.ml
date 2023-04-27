50f341ec336c93da9f1313ac8d5a42ae0d85136d
Evan Williams <evanawilliams@live.com>
2023-04-28 00:42:18 +1000

    😊

--- lib/backoff.ml
+++ lib/backoff.ml
@@ -1,4 +1,12 @@
-type t = unit
+type t = { mutable loops : int }
 
-let create () = (() :> t)
-let backoff _ = ()
+let loops_max = 1000
+let create () = { loops = 1 }
+
+let backoff t =
+  if t.loops >= loops_max then Unix.sleepf (float t.loops /. 1000.0)
+  else (
+    for _ = 1 to t.loops do
+      Caml.Domain.cpu_relax ()
+    done;
+    t.loops <- Int.min (t.loops * 2) loops_max)
